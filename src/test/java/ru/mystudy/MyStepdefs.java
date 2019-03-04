c2a9aac24cc9ccf20b539abdb4a39c7e233302bf
fonbeauty <fonbeauty@mail.ru>
2019-03-05 00:22:49 +0300

    добавлен тест

---
+++ src/test/java/ru/mystudy/MyStepdefs.java
@@ -0,0 +1,22 @@
+package ru.mystudy;
+
+import cucumber.api.java.ru.Дано;
+import cucumber.api.java.ru.Когда;
+import org.testng.Assert;
+
+import java.math.BigDecimal;
+
+public class MyStepdefs {
+    private static BigDecimal cash = new BigDecimal(120000);
+
+
+    @Дано("на счете пользователя имеется {int} рублей")
+    public void наСчетеПользователяИмеетсяРублей(int arg0) {
+        Assert.assertEquals(0, cash.compareTo(new BigDecimal(arg0)));
+    }
+
+    @Когда("пользователь снимает со счета {int} рублей")
+    public void пользовательСнимаетСоСчетаРублей(int arg0) {
+        cash = cash.subtract(new BigDecimal(arg0));
+    }
+}
--- src/test/java/ru/mystudy/RunTest.java
+++ src/test/java/ru/mystudy/RunTest.java
@@ -1,7 +1,9 @@
 package ru.mystudy;
 
+import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
 //@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources/features")
 public class RunTest extends AbstractTestNGCucumberTests {
 }
